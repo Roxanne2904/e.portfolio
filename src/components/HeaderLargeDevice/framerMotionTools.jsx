ff8029fa651e78fae22c7ecda21a3e6966718730
Roxanne <roxanne-viette@hotmail.fr>
2022-09-05 19:30:03 +0200

    responsive design

--- src/components/HeaderLargeDevice/framerMotionTools.jsx
+++ src/components/HeaderLargeDevice/framerMotionTools.jsx
@@ -1,7 +1,7 @@
 export const container = {
-  hidden: { scale: 0, opacity: 0 },
+  hidden: { scaleX: 0, opacity: 0 },
   show: {
-    scale: 1,
+    scaleX: 1,
     opacity: 1,
     transition: { delayChildren: 2, staggerChildren: 0.5 },
   },
--- src/components/List/framerMotionTools.jsx
+++ src/components/List/framerMotionTools.jsx
@@ -1,7 +1,7 @@
 export const item = {
-  hidden: { scale: 0, opacity: 0 },
+  hidden: { scaleX: 0, opacity: 0 },
   show: {
-    scale: 1,
+    scaleX: 1,
     opacity: 1,
     transition: { duration: 0.3, type: "spring", stiffness: 60 },
   },
