d9a7a3a855baa8faa7a04603598edd4a459a8c3a
Roxanne Viette <roxanne-viette@hotmail.fr>
2024-01-04 17:01:13 +0100

    aboutPage: text has been updated

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,82 +3,80 @@
 import Signature from "../../components/Signature/Signature";
 //*styled
 import {
-  StyledParagraphContent,
-  StyledProfilImage,
-  StyledWelcomeMsg,
-  StyledParagraph,
-  StyledLink,
-  StyledSignatureContent,
+	StyledParagraphContent,
+	StyledProfilImage,
+	StyledWelcomeMsg,
+	StyledParagraph,
+	StyledLink,
+	StyledSignatureContent,
+	StyledMediumTitle,
 } from "./styled";
 //*utils
-import { openclassroomsTrainingUrl } from "../../utils/lists/lists";
 import { colors } from "../../utils/GlobalStyled";
 
 export default function About({ width, height }) {
-  return (
-    <StyledParagraphContent
-      width={width}
-      initial={{ scale: 0, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      exit={{ scale: 0, opacity: 0 }}
-      transition={{ duration: 0.7, delay: 0.5 }}
-    >
-      <StyledProfilImage width={width}></StyledProfilImage>
-      <StyledWelcomeMsg width={width}>Bonjour,</StyledWelcomeMsg>
-      <br />
-      <br />
-      <StyledParagraph width={width} height={height}>
-        Après 9 ans d'expérience dans l'entrepreneuriat en tant
-        qu'illustratrice/graphiste mais également peintre décoratrice, j'aspire
-        à ce jour à me réorienter vers une activité professionnelle plus
-        spécifique et concrète. Néanmoins, toujours créative, ma démarche s'est
-        donc dirigée vers un secteur où mes compétences peuvent y être
-        complémentaire. Ayant déjà été intéressé par la création de mon propre
-        site web, c'est naturellement que le métier de développeur front-end a
-        attiré mon attention. Après y avoir suivi{" "}
-        <StyledLink target={"_blank"} href={openclassroomsTrainingUrl}>
-          <strong>la formation chez Openclassrooms</strong>
-        </StyledLink>
-        , je me retrouve à ce jour en recherche d'emploi.
-        <br />
-        <br />
-        Ce qui me plaît dans ce métier, c'est qu'il vise à « donner vie » à une
-        idée. J'y retrouve la même satisfaction que j'ai à peindre ou à
-        dessiner. Au sein de ma formation, au delà de la technique, j'ai appris
-        à me dépasser et à faire face aux difficultés. J'y ai découvert une
-        vraie passion pour le CSS et côté programmation, j'ai très rapidement
-        accroché avec React et Redux. Lors de certains projets, mes compétences
-        de graphiste/ illustratrice ont été utile, par exemple, sur FIGMA, outil
-        de design graphique, pour la création de gabarit. Habituée à communiquer
-        avec d'autre intervenants ou à gérer plusieurs missions simultanément,
-        mon expérience m’a permis de bien cerner qu’un code bien commenté est
-        essentiel, aussi bien pour ses collègues que pour soi. Le bilan de cette
-        expérience n'a fait qu’accroître mon goût pour le développement qui me
-        motive à tenir une veille technologique, entreprendre des projets
-        personnels et à progresser.
-        <br />
-        <br />
-        Au regard de toute mon expérience, et en toute humilité, j’ai bel et
-        bien conscience de l’étendu des connaissances qu’il me reste encore à
-        découvrir. Ce faisant, je me complairais à l’idée de poursuivre cette
-        aventure à travers un nouvel emploi ou des projets.
-        <br />
-        <br />
-        En espérant pouvoir vous captiver davantage à travers ce e-portfolio, je
-        vous souhaite une très bonne visite {"🤗"}
-        .
-        <br />
-        <br />
-        <br />
-        {width > 700 && (
-          <StyledSignatureContent>
-            <Signature color={colors.tertiary} />
-          </StyledSignatureContent>
-        )}
-      </StyledParagraph>
-      {/* <div style={{ transform: "rotate(180deg)" }}>
+	return (
+		<StyledParagraphContent
+			width={width}
+			initial={{ scale: 0, opacity: 0 }}
+			animate={{ scale: 1, opacity: 1 }}
+			exit={{ scale: 0, opacity: 0 }}
+			transition={{ duration: 0.7, delay: 0.5 }}
+		>
+			<StyledProfilImage width={width}></StyledProfilImage>
+			<StyledWelcomeMsg width={width}>Bonjour,</StyledWelcomeMsg>
+			<br />
+			<br />
+			<StyledParagraph width={width} height={height}>
+				<StyledMediumTitle>
+					Développeuse Frontend | Spécialisée en React, TypeScript | Expérience
+					1 an et + chez{" "}
+					<StyledLink target={"_blank"} href={"https://sahar.fr"}>
+						SAHAR
+					</StyledLink>
+				</StyledMediumTitle>
+				<br />
+				<br />
+				Passionnée et compétente développeuse front-end, je cumule plus d'un an
+				d'expérience au sein de SAHAR, une entreprise tournée vers l'innovation.
+				Mon expertise englobe des technologies essentielles telles que React,
+				TypeScript, Redux (en utilisant Redux Toolkit), Storybook, Figma...
+				Cette expérience démontre ma capacité à concevoir et à développer des
+				interfaces utilisateur dynamiques et performantes, alliant savoir-faire
+				technique et créativité.
+				<br />
+				<br />
+				J'ai joué un rôle actif en tant que développeuse frontend. Mes missions
+				incluaient l'intégration de contenus selon les maquettes avec une
+				attention particulière aux détails. J'ai utilisé React-Typescript et les
+				hooks de manière avancée, tout en gérant les versions avec Git et RTK
+				Query pour les appels API.
+				<br />
+				<br />
+				Ma contribution s'est étendue à des projets exigeants en React, où j'ai
+				pu apporter mon expertise. J'ai également pris en charge des chantiers
+				spécifiques, en veillant à maintenir une approche rigoureuse. La
+				participation régulière aux daily scrums a facilité une communication
+				fluide avec l'équipe Backend et les UX/UI designers. Mon respect des
+				deadlines a été un aspect essentiel de mon travail, soulignant mon
+				engagement envers la ponctualité.
+				<br />
+				<br />
+				Cette expérience chez SAHAR a été une étape enrichissante, renforçant
+				mes compétences et ma préparation à relever de nouveaux défis avec
+				humilité et détermination.
+				<br />
+				<br />
+				<br />
+				{width > 700 && (
+					<StyledSignatureContent>
+						<Signature color={colors.tertiary} />
+					</StyledSignatureContent>
+				)}
+			</StyledParagraph>
+			{/* <div style={{ transform: "rotate(180deg)" }}>
         <Corner />
       </div> */}
-    </StyledParagraphContent>
-  );
+		</StyledParagraphContent>
+	);
 }
--- src/pages/About/styled.jsx
+++ src/pages/About/styled.jsx
@@ -1,94 +1,98 @@
 import styled from "styled-components";
 import profileImg from "../../assets/img/cv.jpg";
 import {
-  colors,
-  device,
-  flexColumn,
-  status,
-  position,
-  display,
+	colors,
+	device,
+	flexColumn,
+	status,
+	position,
+	display,
 } from "../../utils/GlobalStyled";
 import { motion } from "framer-motion";
 
 export const StyledParagraphContent = styled(motion.div)`
-  background: ${colors.white};
-  width: ${({ width }) =>
-    width < device.medium
-      ? "100%"
-      : width >= device.XXlarge
-      ? `100%`
-      : "1300px"};
-  ${({ width }) => width < device.medium && "margin: 0 auto"};
-  text-align: justify;
-  box-sizing: ${({ width }) =>
-    width < device.medium ? "content-box" : "border-box;"};
-  box-shadow: ${({ width }) =>
-    width < device.medium
-      ? `inherit`
-      : `0 0 ${width > 1700 ? `5vmin` : `1rem`} ${
-          colors.secondaryDarkerLessO
-        }`};
+	background: ${colors.white};
+	width: ${({ width }) =>
+		width < device.medium
+			? "100%"
+			: width >= device.XXlarge
+			? `100%`
+			: "1300px"};
+	${({ width }) => width < device.medium && "margin: 0 auto"};
+	text-align: justify;
+	box-sizing: ${({ width }) =>
+		width < device.medium ? "content-box" : "border-box;"};
+	box-shadow: ${({ width }) =>
+		width < device.medium
+			? `inherit`
+			: `0 0 ${width > 1700 ? `5vmin` : `1rem`} ${
+					colors.secondaryDarkerLessO
+			  }`};
 
-  padding: ${({ width }) =>
-    width < device.medium
-      ? `2rem 0 0 0`
-      : width >= device.XXlarge
-      ? `5rem 2rem;`
-      : `5rem 2rem 1rem 2rem`};
-  ${({ width }) => width < device.medium && `${flexColumn};align-items:center;`}
+	padding: ${({ width }) =>
+		width < device.medium
+			? `2rem 0 0 0`
+			: width >= device.XXlarge
+			? `5rem 2rem;`
+			: `5rem 2rem 1rem 2rem`};
+	${({ width }) => width < device.medium && `${flexColumn};align-items:center;`}
 `;
 
 export const StyledProfilImage = styled.div`
-  float: ${({ width }) => (width < device.medium ? "inherit" : "left")};
-  background-image: ${({ width }) =>
-    width < device.medium ? "inherit" : `url(${profileImg})`};
-  width: ${({ width }) => (width >= device.XXlarge ? `17.5rem` : `10.5rem`)};
-  height: ${({ width }) =>
-    width < device.medium
-      ? "0vmin"
-      : width >= device.XXlarge
-      ? `25rem`
-      : `15rem`};
-  background-size: 100% 100%;
-  border-radius: 50%;
-  shape-outside: margin-box;
-  margin: ${({ width }) =>
-    width < device.medium ? "30px 0 10px 0" : "0 30px 0 0"};
+	float: ${({ width }) => (width < device.medium ? "inherit" : "left")};
+	background-image: ${({ width }) =>
+		width < device.medium ? "inherit" : `url(${profileImg})`};
+	width: ${({ width }) => (width >= device.XXlarge ? `17.5rem` : `10.5rem`)};
+	height: ${({ width }) =>
+		width < device.medium
+			? "0vmin"
+			: width >= device.XXlarge
+			? `25rem`
+			: `15rem`};
+	background-size: 100% 100%;
+	border-radius: 50%;
+	shape-outside: margin-box;
+	margin: ${({ width }) =>
+		width < device.medium ? "30px 0 10px 0" : "0 30px 0 0"};
 `;
 
 export const StyledWelcomeMsg = styled.span`
-  display: ${({ width }) => (width >= device.XXlarge ? `flex` : `contents`)};
-  font-size: ${({ width }) =>
-    width < device.medium ? "3rem" : width >= 1500 ? `4rem` : "2.8rem"};
-  color: ${colors.tertiary};
-  font-family: auto;
+	display: ${({ width }) => (width >= device.XXlarge ? `flex` : `contents`)};
+	font-size: ${({ width }) =>
+		width < device.medium ? "3rem" : width >= 1500 ? `4rem` : "2.8rem"};
+	color: ${colors.tertiary};
+	font-family: auto;
 `;
 
 export const StyledParagraph = styled.p`
-  font-size: ${({ width, height }) =>
-    width < device.medium
-      ? width < 500
-        ? `1.2rem`
-        : "1.3rem"
-      : width >= device.XXlarge || height > device.large
-      ? `1.5rem`
-      : "1.1rem"};
-  padding: ${({ width }) => (width < device.medium ? "25px" : " 10px")};
+	font-size: ${({ width, height }) =>
+		width < device.medium
+			? width < 500
+				? `1.2rem`
+				: "1.3rem"
+			: width >= device.XXlarge || height > device.large
+			? `1.5rem`
+			: "1.1rem"};
+	padding: ${({ width }) => (width < device.medium ? "25px" : " 10px")};
 
-  ${({ width }) => width < device.medium && "overflow: scroll;"}
+	${({ width }) => width < device.medium && "overflow: scroll;"}
 `;
 
 export const StyledLink = styled.a`
-  color: ${colors.tertiary};
-  text-decoration: ${status.none};
-  &:hover {
-    color: ${colors.primary};
-  }
+	color: ${colors.tertiary};
+	text-decoration: ${status.none};
+	&:hover {
+		color: ${colors.primary};
+	}
 `;
 export const StyledSignatureContent = styled.span`
-  display: ${display.flex};
-  position: ${position.relative};
-  justify-content: end;
-  margin: 0 50px 0 50px;
-  bottom: 20px;
+	display: ${display.flex};
+	position: ${position.relative};
+	justify-content: end;
+	margin: 0 50px 0 50px;
+	bottom: 20px;
+`;
+
+export const StyledMediumTitle = styled.h2`
+	font-size: 1.2rem;
 `;
